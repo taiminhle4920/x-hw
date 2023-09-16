88d075f097388886da436e21ae2f4ef9068ab03c
Haoming Chen <haomingchen@Haomings-Air.attlocal.net>
2023-09-15 22:42:51 -0700

    added test cases

--- test_api.py
+++ test_api.py
@@ -9,29 +9,23 @@ class Apptest(unittest.TestCase):
                   "id": 0}
 
  #check for response 200
-    def test_index(self):
+    def test_homepage_(self):
         tester = app.test_client(self)
         response = tester.get("/")
         statuscode = response.status_code
         self.assertEqual(statuscode, 200)
-    
-    def test_index_content(self):
-        tester = app.test_client(self)
-        response = tester.get("/")
-        html = response.data.decode("utf8")
-        self.assertIn("Create a Tweet", html)  
-        self.assertIn("Deleta a Tweet", html)
+        print("test homepage complete")
 
     #tweet empty text
-    def test_create_tweet_api(self):
+    def test_create_tweet_api_1(self):
         tester = app.test_client(self)
         resp = tester.post("/tweet", data="")
         resp_json = json.loads(resp.data.decode('utf-8'))
         self.assertEqual("content is empty", resp_json["error"])
-        print("test case 1 complete")
+        print("create test case 1 complete")
 
     #tweet random text
-    def test_create_tweet_api(self):
+    def test_create_tweet_api_2(self):
         tester = app.test_client(self)
         resp = tester.post("/tweet", data='text='+self.test_data['text'])
         resp_json = json.loads(resp.data.decode('utf-8')) 
@@ -40,30 +34,29 @@ class Apptest(unittest.TestCase):
         print("create test case 2 complete")
     
     #delete empty id
-    def test_delete_tweet_api(self):
+    def test_delete_tweet_api_1(self):
         tester = app.test_client(self)
         resp = tester.delete("/delete?tweetId=")
         resp_json = json.loads(resp.data.decode('utf-8'))
         self.assertEqual("tweetId is empty", resp_json["error"])
-        print("delete test case 3 complete")
+        print("delete test case 1 complete")
     
     #delete wrong id
-    def test_delete_tweet_api(self):
+    def test_delete_tweet_api_2(self):
         tester = app.test_client(self)
         resp = tester.delete("/delete?tweetId=$100")
         resp_json = json.loads(resp.data.decode('utf-8'))
         self.assertEqual("tweetID not found", resp_json["error"])
-        print("delete test case 4 complete")
+        print("delete test case 2 complete")
     
     #delete existing id
-    def test_delete_tweet_api(self):
+    def test_delete_tweet_api_3(self):
         tester = app.test_client(self)
         delete_id = self.test_data['id']
         resp = tester.delete("/delete?tweetId="+str(delete_id))
         resp_json = json.loads(resp.data.decode('utf-8'))
-        # self.assertTrue(b"success" in resp.data)
         self.assertEqual("tweet deleted", resp_json["success"])
-        print("delete test case 5 complete")
+        print("delete test case 3 complete")
 
     
 if __name__=="__main__":
